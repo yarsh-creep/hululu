9593b45ee32c430b0b9ea155c9f4ea58bd1858a4
yashdev <yarshdrive@gmail.com>
2023-08-19 01:32:41 +0530

    index file added

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+   
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="index.css">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+          
+    <head>
+      <link rel="icon" type="image/x-icon" href="../images/llllll.png">
+
+    </head>
+    
+    <body>
+<!--  navbar        -->
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top" id="header">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html"><img src="../images/llllll.png"  height="100px" width="100px" style="margin-top: 10px;" ><b style="font-size: 40px;">AD SALON</b></a>
+      <center>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse"  id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" style="color:white;font-size: 20px;" href="index.html">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color:white;font-size: 20px;" href="product.html">PRODUCT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color:white;font-size: 20px;" href="services.html">SERVICES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link bar" style="color:rgb(0, 0, 35); background-color:#fab22c;border-radius: 6px;height: 42px;font-size: 20px;" href="booking.html">BOOKING</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link" style="color:white;font-size: 20px;" href="contact.html">CONTACT</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </li>
+        </ul>
+      </div></center>
+    </div>
+  </nav>
+<!--/nav  -->
+<!-- video -->
+<div class="container-fluid">  
+    <video class="video" autoplay loop muted width="100%" height="500%"  >
+      <source src="../images/production ID_4178109.mp4" type="video/mp4">
+      </video>
+  </div>
+<!--/video  -->
+
+<!--hr line  -->
+<hr class="dashed">
+<!--/hr line  -->
+<!-- jumbotron  -->
+      <div class="container-fluid mt-3 logo">
+      <div class="mt-4 p-5 text-white rounded shadow p-4 mb-4 bg-black" style="background-image: linear-gradient( 90deg , rgba(7, 0, 31, 0.651) 100px, rgba(0, 0, 33, 0.937));">
+          <h1>OUR GOAL</h1> 
+          <p>is to enhance the beauty of each and every person, we started this business on 5th september 2020 and it has been running since 3 years we strive to reach beyond the roots ,and into the refinement and the healing of one's core self.we aim to offer customized haircuts and colors correction to strike the perfect balance of look are enduring yet original.us as ateam are simple,well-experienced and creative with our scissors.
+            Glow & Glamour, the cosmetic shop near Kwality More, has an entire eye-level shelf dedicated to men. It has Beardo beard softener, Ustraa beard wash, beard growth oil, de-tanning cream, as well as ayurvedic Move over ladies! The 21st century man is shattering gender stereotypes and barging into a domain traditionally dominated by women — grooming.Our talented and professional hair stylists assure the best of services, aiming for a higher level of customer satisfaction.</p> 
+          <!-- progess bar -->
+          <div class="progress" style="height:5px">
+            <div class="progress-bar" style="width:100%;height:10px;background-image: linear-gradient( 90deg , rgba(22, 87, 160, 0.651) 100px, rgba(244, 153, 194, 0.937)); "></div>
+          </div>
+          </div><br><br>
+          <div class="d-flex flex-column justify-content-center w-100 h-100" id="body">
+
+            <div class="d-flex flex-column justify-content-center align-items-center">
+              <h1 class="fw-light text-white m-0"><b> AD SALON WE ARE MEANT TO STRIVE </b></h1><br><br>
+              <!-- <div class="btn-group my-5">
+                <a href="https://codepen-api-export-production.s3.us-west-2.amazonaws.com/zip/PEN/pyBNzX/1578778289271/pure-css-gradient-background-animation.zip" class="btn btn-outline-light" aria-current="page"><i class="fas fa-file-download me-2"></i> SOURCE CODE</a>
+                <a href="https://codepen.io/P1N2O/full/pyBNzX" class="btn btn-outline-light">FULL SCREEN <i class="fas fa-expand ms-2"></i></a>
+              </div>-->
+              <a href="https://manuel.pinto.dev" class="text-decoration-none">
+                <h5 class="fw-light text-white m-0"><img src="images/llllll.png" width="400px" height="400px"></h5>
+              </a>
+            </div>
+          </div>
+          </div>
+      </div>
+<!-- /jumbotron about us -->
+
+<!--  watsapp  -->
+<a  class="whats-app" href="https://chat.whatsapp.com/JJD4oh2yPC28jLyj1LBhRi" target="_blank" style="text-decoration:none;color: white;">
+  <i class="fa fa-whatsapp my-float"></i>
+</a>
+<!-- watsapp end -->
+<!--  facebook  -->
+<a  class="facebook" href="https://www.facebook.com/profile.php?id=100088560454186 " target="_blank" style="text-decoration:none;color: white;">
+  <i class="fa fa-facebook float" style="margin-left: 20px;"></i>
+</a>
+<!-- facebook end -->
+
+<!--  instagram  -->
+<a  class="instagram" href="https://instagram.com/saloon_ad?igshid=YmMyMTA2M2Y= " target="_blank" style="text-decoration:none;color: white;">
+  <i class="fa fa-instagram free"></i>
+</a>
+<!-- instagram end -->
+
+<!--slidebar for services-->
+<!-- <div class="offcanvas offcanvas-start" id="demo" style="background-image: linear-gradient( 90deg , rgba(7, 0, 31, 0.651) 100px, rgba(0, 0, 33, 0.937));color: white;">
+  <div class="offcanvas-header">
+    <h1 class="offcanvas-title">WE PROVIDE SERVICES</h1>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>HAIR SERVICES</p>
+    <img src="images/j1.jpg" width="200px" height="200px" class="zoom" >
+    <p>BEARD SERVICES</p>
+    <img src="images/j2.jpg" width="200px" height="200px" class="zoom">
+    <p>MASSAGE SERVICES</p>
+    <img src="images/j3.jpg" width="200px" height="200px" class="zoom">
+    <p>FACIAL</p>
+    <img src="images/j4.jpg"width="200px" height="200px" class="zoom"><br><br>
+  <a href="booking.html"><button class="btn btn-secondary" type="button">!BOOKING!</button></a>
+  </div>
+</div> -->
+<!--/slidebar  -->
+<!-- about us --><br><br><br>
+<h1 align="center">GALLERY</h1> 
+<hr>
+
+<div class="container-fluid mt-3">
+    <div class=" img-fluid about a">
+      <img class="img-top a" src="images/Untitled design (2).png" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+    </div>
+
+    <div class=" img-fluid about a" >
+      <img class="img-top a" src="images/Untitled design (8).png" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+    </div>
+
+    <div class="img-fluid about b" >
+      <img class="img-top b" src="images/Untitled design (3).png" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="card-text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+    </div>
+
+    <div class="img-fluid about c" >
+      <img class="img-top c" src="images/z3.jpg" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+    </div>
+
+    <div class="img-fluid about  d" >
+      <img class="img-top d " src="images/ss6.jpg" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="card-text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+    </div>
+
+
+    <div class="img-fluid about  e" >
+      <img class="img-top e" src="images/massagestone.png" alt="Card image" style="width:100%;background-image: linear-gradient( 90deg , rgb(40, 219, 195) 100px, rgba(0, 0, 33, 0.937));">
+      <div class="img-overlay">
+        <h4 class="title" style="color:rgba(0, 0, 33, 0.937);"></h4>
+        <br><br><br><br><br>
+        <p class="card-text" style="color:rgba(0, 0, 33, 0.937);text-align:justify;"></p>
+        </div>
+      </div>
+      
+    </div>
+
+<!-- <div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div> -->
+
+
+<!-- <div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div> -->
+<!-- <div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>
+<div class="container" align="center"><div class="div"></div> <br> <div class="div"></div></div>  -->
+
+  
+<!-- /image card about us -->
+<!-- Carousel -->
+
+<!-- <div id="demo" class="carousel slide" data-bs-ride="carousel">
+   -->
+  <!-- Indicators/dots -->
+  <!-- <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  </div> -->
+  
+  <!-- The slideshow/carousel -->
+  <!-- <br><br><br>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/profile1.jpeg" alt="Los Angeles" class="d-block" style="width:80%;height: 60%;margin-left: 50px">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/profile2.jpg" alt="Chicago" class="d-block" style="width:80% ;height: 60%;margin-left: 50px;">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div> 
+    </div>
+    <div class="carousel-item">
+      <img src="images/profile3.jpeg" alt="New York" class="d-block" style="width:80%;height: 60%;margin-left: 50px;">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>  
+    </div>
+  </div>
+   -->
+  <!-- Left and right controls/icons -->
+  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+</div>-->
+<br><br> 
+<!--hr line  -->
+<!-- <hr class="dashed"> -->
+<!--/hr line  -->
+<!-- jumbotron  -->
+      <div class="container-fluid mt-3 logo">
+      <!-- <div class="mt-4 p-5 text-white rounded shadow p-4 mb-4 bg-black" style="background-image: linear-gradient( 90deg , rgba(7, 0, 31, 0.651) 100px, rgba(0, 0, 33, 0.937));">
+          <h1>OUR GOAL</h1> 
+          <p>is to enhance the beauty of each and every person, we started this business on 5th september 2020 and it has been running since 3 years we strive to reach beyond the roots ,and into the refinement and the healing of one's core self.we aim to offer customized haircuts and colors correction to strike the perfect balance of look are enduring yet original.us as ateam are simple,well-experienced and creative with our scissors.
+            Glow & Glamour, the cosmetic shop near Kwality More, has an entire eye-level shelf dedicated to men. It has Beardo beard softener, Ustraa beard wash, beard growth oil, de-tanning cream, as well as ayurvedic Move over ladies! The 21st century man is shattering gender stereotypes and barging into a domain traditionally dominated by women — grooming.Our talented and professional hair stylists assure the best of services, aiming for a higher level of customer satisfaction.</p> 
+           progess bar 
+          <div class="progress" style="height:5px">
+            <div class="progress-bar" style="width:100%;height:10px;background-image: linear-gradient( 90deg , rgba(22, 87, 160, 0.651) 100px, rgba(244, 153, 194, 0.937)); "></div>
+          </div>
+          </div><br><br> -->
+          <div class="d-flex flex-column justify-content-center w-100 h-100" id="body">
+
+            <div class="d-flex flex-column justify-content-center align-items-center">
+              <h1 class="fw-light m-0" style="color: rgba(0, 0, 33, 0.937);"><b> “Don't forget to pamper yourself.” </b></h1><br><br>
+              <!-- <div class="btn-group my-5">
+                <a href="https://codepen-api-export-production.s3.us-west-2.amazonaws.com/zip/PEN/pyBNzX/1578778289271/pure-css-gradient-background-animation.zip" class="btn btn-outline-light" aria-current="page"><i class="fas fa-file-download me-2"></i> SOURCE CODE</a>
+                <a href="https://codepen.io/P1N2O/full/pyBNzX" class="btn btn-outline-light">FULL SCREEN <i class="fas fa-expand ms-2"></i></a>
+              </div>-->
+              <!-- <a href="https://manuel.pinto.dev" class="text-decoration-none">
+                <h5 class="fw-light text-white m-0"><img src="images/llllll.png" width="400px" height="400px"></h5>
+              </a> -->
+            </div>
+          </div>
+          </div>
+      </div><br><br>
+<!-- /jumbotron about us -->
+
+<!--footer 1  -->
+<div>
+<div class="container-fluid">
+  <footer id="footer" class="footer-1">
+    <div class="main-footer widgets-dark typo-light">
+    <div class="container">
+    <div class="row">
+      
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget subscribe no-box">
+    <h5 class="widget-title">AD SALON<span></span></h5>
+    <p>Our goal is to try to enhance the beauty of each and every person.</p>
+    </div>
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget no-box">
+    <h5 class="widget-title">PAGES IN THIS WEBSITE<span></span></h5>
+        <a href="home.html" style="text-decoration:none;color: white;">HOME</a><br>
+        <a href="product.html" style="text-decoration:none;color: white;">PRODUCT</a><br>
+        <a href="services.html" style="text-decoration:none;color: white;">SERVICES</a><br>
+        <a href="booking.html" style="text-decoration:none;color: white;">BOOKING</a><br>
+        <a href="contact.html" style="text-decoration:none;color: white;">CONTACT</a>
+    </div>
+    
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    <div class="widget no-box">
+    <h5 class="widget-title">SET AN  APPOINTMENT<span></span></h5>
+    <p>Groom yourself with us </p>
+    <a class="btn" href="booking.html" target="_blank" style="background-color: white;color:rgb(0, 0, 35);">BOOK NOW</a>
+    </div>
+    
+    </div>
+    
+    <div class="col-xs-12 col-sm-6 col-md-3">
+    
+    <div class="widget no-box">
+    <h5 class="widget-title">Contact Us<span></span></h5>
+    
+    <p><a href="adsaloon2020@gmail.com" title="glorythemes" style="text-decoration:none; color:white;">adsaloon2020@gmail.com.com</a></p>
+    <p><a  style="text-decoration:none; color:white">8055578837</a></p>
+  
+    <ul class="social-footer2">
+        <a href="https://instagram.com/saloon_ad?igshid=YmMyMTA2M2Y=" style="color:white;"><i class="fa fa-instagram" aria-hidden="true" style="font-size: 24px ; text-decoration: none;"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+        
+        <a href="https://www.facebook.com/profile.php?id=100088560454186 " style="color:white ;"><i class="fa fa-facebook" aria-hidden="true" style="font-size: 24px ;"></i></a>
+    </ul>
+   
+    </div></div>
+   
+    </div></div>
+    
+    </div></div>
+    </div></div>
+    </div>
+    <hr></hr>
+    <div align="center">copyright &#9400;by ashley and yash</div>
+  </footer>
+     
+
+  <!--/footer 1  -->
+    </body>
+</html>
